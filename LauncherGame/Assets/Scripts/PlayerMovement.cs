29fc17872ad30cfcc03dbfe8d4220e01f2d5c9ac
RvalleJ100 <rvalle@umassd.edu>
2023-02-19 01:29:31 -0500

    Rocket Jump Initial Prototype
    Currently a super jump, but calculates angle (both radians and degrees) between the mouse position and the player. Also judges distance of x and y values between the entities as well.

--- LauncherGame/Assets/Scripts/PlayerMovement.cs
+++ LauncherGame/Assets/Scripts/PlayerMovement.cs
@@ -12,9 +12,16 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private LayerMask jumpableGround;
 
+    // MOVEMENT FORCE SECTION
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
+
+    // ROCKET JUMP SECTION
+    private bool rocketReady;
+    private float rocketCD = 1.2f;
+    private float rocketCDCurrent = 0.0f;
+    private Transform launchDirection;
 
     private enum MovementState { idle, running, jumping, falling }
 
@@ -33,11 +40,31 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
+        // IF PLAYER JUMPS
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
+        // ROCKET JUMP COOLDOWN CHECK
+        if(rocketCDCurrent >= rocketCD)
+        {
+            rocketReady = true;
+        }
+        else
+        {
+            rocketCDCurrent = rocketCDCurrent + Time.deltaTime;
+            rocketReady = false;
+        }
+
+        // IF PLAYER USES ROCKET JUMP
+        if (Input.GetMouseButtonDown(0) && rocketReady)
+        {
+            // RESTART COOLDOWN AND LAUNCH PLAYER
+            rocketCDCurrent = 0.0f;
+            RocketLaunchPlayer();
+        }
+        
         UpdateAnimationState();
     }
 
@@ -75,5 +102,22 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
+    // ROCKET JUMP CALCULATIONS AND LAUNCH
+    private void RocketLaunchPlayer()
+    {
+        Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log("Mouse: " + cursorPosition);
+        Debug.Log("RB: " + rb.transform.position);
+
+        // GET DIFFERENCE BETWEEN MOUSE AND PLAYER POSITION AND GET ANGLE IN RADIANS/DEGREES
+        float xdif = cursorPosition.x - rb.transform.position.x;
+        float ydif = cursorPosition.y - rb.transform.position.y;
+        float anglerad = Mathf.Atan2(xdif,ydif);                    // ANGLE IN RADIANS
+        float angledeg = Mathf.Atan2(xdif,ydif) * Mathf.Rad2Deg;    // ANGLE IN DEGREES
+        Debug.Log("angle: " + angledeg);
+        // DEFAULT VELOCITY (FOR NOW) THAT IS BASICALLY A SUPER JUMP
+        rb.velocity = new Vector2(rb.velocity.x*2, jumpForce*2);
     }
 }
