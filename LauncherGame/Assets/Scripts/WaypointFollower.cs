5edecdc2e21328ec5d31407f05d19a1eb8ca44c4
jbader2001 <77864628+jbader2001@users.noreply.github.com>
2023-04-23 16:54:55 -0400

    commented code, deleted old scripts

--- LauncherGame/Assets/Scripts/WaypointFollower.cs
+++ LauncherGame/Assets/Scripts/WaypointFollower.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class WaypointFollower : MonoBehaviour
 {
+    //create a list to hold all the waypoint gameobjects
     [SerializeField] private GameObject[] waypoints;
+    //variable to hold the current waypoint to move towards
     private int currentWaypointIndex = 0;
     [SerializeField] private float speed = 2f;
 
     // Update is called once per frame
     void Update()
     {
+        //if within 0.1 units of the current target waypoint, switch to the next waypoint in the list
+        //if there is no next target waypoint, start over at the beginning of the list
         if(Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < 0.1f)
         {
             currentWaypointIndex++;
@@ -19,6 +23,7 @@ public class WaypointFollower : MonoBehaviour
                 currentWaypointIndex = 0;
             }
         }
+        //move towards the next target waypoint
         transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
     }
 }
